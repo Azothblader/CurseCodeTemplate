57611306e4fb0adf48b525a64a87a8bb16a50d68
Azothbader <necross_fiction@hotmail.com>
2022-07-22 00:22:35 +0700

    Add more detail text

--- Program.cs
+++ Program.cs
@@ -5,25 +5,33 @@ Console.WriteLine("Hello, World!");
 
 var coreL = new CoreLogic();
 var input = new CoreLogic.CoreInput();
+input.inputVal1="Initial no output value";
 var serviceResult = new CoreLogic.CoreOutput("");
 
+// Original Core Logic 
+    serviceResult = coreL.DoOperaton(input);
+    Console.WriteLine("Initial CoreLogic : " + serviceResult.outValue);
 
+#region Test : ขอเซ็ทหย่อ 
 // Test : ขอเซ็ทหย่อ using v0.2  
+Console.WriteLine("Test : ขอเซ็ทหย่อ using v0.2");
     var นักโทษ = new คนคุก(coreL);
+    input.inputVal1 ="แล้วคุณจะรู้มั้ย ว่าเค้าจะเล่นคุณ!";
     // #1   
     var ไข่1  = input ;
     serviceResult = นักโทษ.ขอเซ็ทหย่อ.สู่ต่อ.ซูดผ่อ.ซีหม่อ.สองห่อ.ใส่(ไข่1);
-    Console.WriteLine("#1 : " + serviceResult.outValue);
+    Console.WriteLine("   #1 : " + serviceResult.outValue);
     // #2 
     Action<CoreLogic.CoreInput> UwU =  
      ( x )   => { var ไข่2 = x; serviceResult = นักโทษ.ขอเซ็ทหย่อ.สู่ต่อ.ซูดผ่อ.ซีหม่อ.สองห่อ.ใส่(ไข่2); };
     UwU(ไข่1); 
-    Console.WriteLine("#2 : " + serviceResult.outValue);
-
+    Console.WriteLine("   #2 : " + serviceResult.outValue);
     // #3
     Func< CoreLogic.CoreInput, CoreLogic.CoreOutput> tempF =
      ( x ) => {return นักโทษ.ขอเซ็ทหย่อ.สู่ต่อ.ซูดผ่อ.ซีหม่อ.สองห่อ.ใส่(x);};
-    Console.WriteLine("#3 : " + tempF(ไข่1).outValue );
+    Console.WriteLine("   #3 : " + tempF(ไข่1).outValue );
+
+#endregion
 
 
 Console.WriteLine("End Test");
